9f02d3b5e84c0dab0d21c08e69ef42cbf9af284d
Aizaz Ahmad <aizaz1996@mail.fresnostate.edu>
2021-02-24 03:43:11 +0000

    SleepinFunction

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+#Coding Bat Exercises
+#Function Sleep in 2 arguments weekday and vacation boolean arguments
+#True we sleep in weekday false or vacation True
+
+def sleep_in (weekday,vacation):
+  if weekday == False or vacation == True:
+    return True
+  else:
+    return False
+
+  
